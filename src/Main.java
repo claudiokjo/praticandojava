aa2367e89f6039d5f1fd97993eb9d4f1e94b04db
Adriley Samuel Ribeiro Barbosa <asbarbosa@stefanini.com>
2023-08-10 19:56:58 -0300

    Loop for added

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("zags!");
+        for(i=0; i < 10; i++){
+            System.out.printf("zags!");
+        }
 
     }
 }
